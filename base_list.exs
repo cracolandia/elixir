1fda7363e12f6fc3fd8a0be69cbc9bbdeb2458ab
sylviot <sylvio.tavares@hotmail.com>
2016-04-01 19:15:10 -0300

    Base math

--- base_list.exs
+++ base_list.exs
@@ -1,96 +1,86 @@
 # Module
 
-defmodule Base do
-	def list_head(list) do
+defmodule Base.List do
+	def head(list) do
 		[head | _] = list
 		head
 	end
 
-	def list_tail(list) do
+	def tail(list) do
 		[_ | tail] = list
 		tail
 	end
 
-	def list_except(list, e) do
+	def except(list, e) do
 		Enum.filter list, &_EXCEPT(&1, e)
 	end
 
-	def list_filter(list, func) do
-		Enum.filter list, func
-	end
+	def odd(list), do: Enum.filter list, &_ODD/1
+	
+	def even(list), do: Enum.filter list, &_EVEN/1
 
-	def list_greater_than(list, e) do
+	def greater_than(list, e) do
 		Enum.filter list, &_GREATER_THAN(&1, e)
 	end
 
-	def list_less_than(list, e) do
+	def less_than(list, e) do
 		Enum.filter list, &_LESS_THAN(&1, e)
 	end
 
 	# FUNC
-	def _ODD(x) do
-		!_EVEN(x)
-	end
+	def _EVEN(x), do: rem(x, 2) == 0
+	def _EXCEPT(x, e), do: x != e
+	def _GREATER_THAN(x, e), do: x > e
+	def _LESS_THAN(x, e), do: x < e
+	def _ODD(x), do: !_EVEN(x)
 
-	def _EVEN(x) do
-		rem(x, 2) == 0
-	end
-
-	def _EXCEPT(x, e) do
-		x != e
-	end
-
-	def _GREATER_THAN(x, e) do
-		x > e
-	end
-
-	def _LESS_THAN(x, e) do
-		x < e
-	end
 end
 
 # Tests
 
 ExUnit.start
 
-defmodule BaseTest do
+defmodule Base.List.Test do
 	use ExUnit.Case
 
-	def list_string do
+	def string do
 		["a", "b", "c"]
 	end	
 
-	def list_integer do
+	def integer do
 		[1, 2, 3]
 	end
 
 	test 'head' do
-		assert Base.list_head(list_string) == "a"
-		assert Base.list_head(list_integer) == 1
+		assert Base.List.head(string) == "a"
+		assert Base.List.head(integer) == 1
 	end
 
 	test 'tail' do
-		assert Base.list_tail(list_string) == ["b", "c"]
-		assert Base.list_tail(list_integer) == [2, 3]
+		assert Base.List.tail(string) == ["b", "c"]
+		assert Base.List.tail(integer) == [2, 3]
 	end
 
 	test 'except' do
-		assert Base.list_except(list_string, "b") == ["a", "c"]
-		assert Base.list_except(list_integer, 2) == [1, 3]
+		assert Base.List.except(string, "b") == ["a", "c"]
+		assert Base.List.except(integer, 2) == [1, 3]
 	end
 
 	test 'greater_than' do
-		assert Base.list_greater_than(list_string, "a") == ["b", "c"]
-		assert Base.list_greater_than(list_integer, 1) == [2, 3]
+		assert Base.List.greater_than(string, "a") == ["b", "c"]
+		assert Base.List.greater_than(integer, 1) == [2, 3]
 	end
 
 	test 'less_than' do
-		assert Base.list_less_than(list_string, "c") == ["a", "b"]
-		assert Base.list_less_than(list_integer, 3) == [1, 2]
+		assert Base.List.less_than(string, "c") == ["a", "b"]
+		assert Base.List.less_than(integer, 3) == [1, 2]
 	end
 
-	test 'filter_with_function' do
-		assert Base.list_filter(list_integer, &Base._ODD/1) == [1, 3]
-		assert Base.list_filter(list_integer, &Base._EVEN/1) == [2]
+	test 'filter_odd' do
+		assert Base.List.odd(integer) == [1, 3]
+	end
+
+	test 'filter_even' do
+		assert Base.List.even(integer) == [2]
 	end
 end
